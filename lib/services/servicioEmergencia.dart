0fa806f65ec3fb841190e3e8994ff4b1f8638cc0
GabrielFMH <84587604+GabrielFMH@users.noreply.github.com>
2025-04-16 16:10:03 -0500

    Update servicioEmergencia.dart

---
+++ lib/services/servicioEmergencia.dart
@@ -0,0 +1,10 @@
+class EmergencyService {
+  static List<Map<String, String>> getEmergencyContacts() {
+    return [
+      {'name': 'Policía', 'phone': '911'},
+      {'name': 'Bomberos', 'phone': '912'},
+      {'name': 'Ambulancia', 'phone': '913'},
+      {'name': 'Protección Civil', 'phone': '914'},
+    ];
+  }
+}
